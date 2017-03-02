c567e61c9e57600a27f6406d82f393cc90cdbb53
Sjoerd Lohuis <git@initwp.com>
2017-03-02 09:22:24 +0100

    Mailto anti spambot

---
+++ mailto-anti-spambot.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Hide/obfuscate email adresses from Spam Bots using a shortcode
+ * Example: [email]info@email.com[/email] or [email mailto="info@email.com"]Send us a message[/email]
+ *
+ * @param array  $atts    Shortcode attributes.
+ * @param string $content The shortcode content. Should be an email address.
+ *
+ * @return string The obfuscated email address.
+ */
+function TEXTDOMAIN_hide_email_shortcode( $atts , $content = null ) {
+
+	$a = shortcode_atts( array(
+		'mailto' => $content,
+        'class' => '',
+    ), $atts );
+
+	if ( !is_email( $content ) && !is_email( $a['mailto'] )) {
+		return;
+	}
+
+	if ($a['class']) {
+		$class = 'class="' . $a['class'] . '"';
+	} else {
+		$class = '';
+	}
+
+	return '<a href="mailto:' . antispambot( $a['mailto'] ) . '" ' . $class . '>' . antispambot( $content ) . '</a>';
+}
+add_shortcode( 'email', 'TEXTDOMAIN_hide_email_shortcode' );
